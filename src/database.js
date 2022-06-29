cd133cd9e59be10d40af32bce52cc1dfcc75d37d
Juan Gonzalez <juangonzaleze04@gmail.com>
2022-06-29 10:52:59 -0400

    last change

--- src/database.js
+++ src/database.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 require('dotenv').config();
-console.log('here...', process.env.MONGODB_URI_LOCAL)
+console.log('here...', process.env.MONGODB_URL)
 
 // local mongodb://localhost/api_db;
 
-mongoose.connect(process.env.MONGODB_URI_LOCAL, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser            : true,
     useUnifiedTopology         : true,
     useFindAndModify           : false,
