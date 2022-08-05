adffebcd5cb86a22a0ddd53c08a99c6fa961a5f2
Juan Gonzalez <juangonzaleze04@gmail.com>
2022-08-05 13:04:12 -0400

    change

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ import path from 'path';
 const app = express();
 
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true,limit: '50mb' }));
+app.use(express.urlencoded({ extended: true,limit: '50mb', parameterLimit: 50000 }));
 app.use(cors());
 app.options('*', cors());
 app.use(morgan('dev'));
