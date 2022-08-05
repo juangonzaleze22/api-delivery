7ccc5cd690fed9b3433acf534ef1cf48c6508765
Juan Gonzalez <juangonzaleze04@gmail.com>
2022-08-05 13:02:01 -0400

    change config

--- src/app.js
+++ src/app.js
@@ -15,7 +15,8 @@ import path from 'path';
 
 const app = express();
 
-app.use(bodyParser.json({limit: '100mb'}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true,limit: '50mb' }));
 app.use(cors());
 app.options('*', cors());
 app.use(morgan('dev'));
